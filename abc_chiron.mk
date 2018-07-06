50f4fe53006c243c7aa3be86982697b7b9db3077
Vol Zhdanov <wight554@gmail.com>
2018-07-06 15:55:21 +0000

    chiron: abc-ify

--- abc_chiron.mk
+++ abc_chiron.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
@@ -15,3 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
