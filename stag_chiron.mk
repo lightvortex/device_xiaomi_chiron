bc5778ffbb0dd5d8390be09f586f7d2807798b00
lightvortex <rishishukla242@gmail.com>
2019-02-03 11:28:58 +0530

    Stag-ify

--- stag_chiron.mk
+++ stag_chiron.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 $(call inherit-product, vendor/havoc/config/common.mk)
 
-PRODUCT_NAME := havoc_chiron
+PRODUCT_NAME := stag_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
@@ -17,6 +17,3 @@ BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
 
-
-# Official HavocOS
-HAVOC_BUILD_TYPE := Official
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -3,7 +3,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qti.sensors.noneui=true \
 	ro.xiaomi.sar_fusion=true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-        ro.havoc.maintainer=LightVortex
-	ro.sf.lcd_density=440
 
